9220b61c803cc31610bfc490fdbbd85091a7b2f3
Mehul Chaudhari <chaudharimehul@gmail.com>
2017-06-07 08:28:19 -0700

    Graph v1
    - Version 1 of Graph
    - Insert operation
    - BFS
    - Will revisit this for code clean up and large data set

--- practice-world/src/com/compit/programming/basics/datastructure/graph/GraphNode.java
+++ practice-world/src/com/compit/programming/basics/datastructure/graph/GraphNode.java
@@ -1,16 +1,12 @@
 package com.compit.programming.basics.datastructure.graph;
 
-import java.util.List;
-
 /**
  * @author: chaudharimehul
  * @date:	May 30, 2017
  * 
  */
 public class GraphNode {
-	
 	int data;
-	List<GraphNode> neighbourNodes;
 	
 	public GraphNode(){
 		
@@ -18,12 +14,6 @@ public class GraphNode {
 	
 	public GraphNode(int data){
 		this.data = data;
-		this.neighbourNodes = null;
-	}
-	
-	public GraphNode(int data, List<GraphNode> neighbourNodes){
-		this.data = data;
-		this.neighbourNodes = neighbourNodes;
 	}
 	
 	public int getData(){
@@ -32,13 +22,5 @@ public class GraphNode {
 	
 	public void setData(int data){
 		this.data = data;
-	}
-	
-	public void setNeighbourNodes(List<GraphNode> neighbourNodes){
-		this.neighbourNodes = neighbourNodes;
-	}
-	
-	public List<GraphNode> getNeighbourNodes(){
-		return this.neighbourNodes;
 	}
 }
--- practice-world/src/com/compit/programming/basics/datastructure/graph/Graphv1.java
+++ practice-world/src/com/compit/programming/basics/datastructure/graph/Graphv1.java
@@ -1,6 +1,10 @@
 package com.compit.programming.basics.datastructure.graph;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+
+import com.compit.programming.basics.datastructure.questack.Queuev1;
 
 /**
  * @author: chaudharimehul
@@ -8,28 +12,64 @@ import java.util.List;
  * 
  */
 public class Graphv1 {
-
-	GraphNode startNode;
-	List<GraphNode> neighbourNode;
-
 	
-	public GraphNode getStartNode(){
-		return this.startNode;
-	}
+	List<GraphNode> nodeList;
+	LinkedList<Integer> edge[];
+	int nodeID;
 	
-	public void setStartNode(GraphNode startNode){
-		this.startNode = startNode;
-	}
+	int numberOfVertices = 10;
 	
-	public void bfs(){
+	public Graphv1(int data)
+	{
+		this.nodeList = new ArrayList<GraphNode>();
+		this.edge = new LinkedList[numberOfVertices];
+		for(int i = 0 ; i < numberOfVertices ; i++){
+			this.edge[i] = new LinkedList<Integer>();
+		}
+		this.nodeID = 0;
 		
+		GraphNode node = new GraphNode(data);
+		addNode(node);
+	}
+	public int getNodeID(){
+		return this.nodeID;
+	}
+	void addNode(int data){
+		addNode(new GraphNode(data));
+	}
+	void addNode(GraphNode node){
+		this.nodeList.add(this.nodeID, node);
+		increamentNodeID();
+	}
+	
+	void addEdge(int nodeIdA, int nodeIdB){
+		edge[nodeIdA].add(nodeIdB);
+	}
+	
+	public void increamentNodeID(){
+		this.nodeID++;
+	}
+	
+	public void bfs(int nodeID){
+		Queuev1 queue = new Queuev1();
+		boolean[] visited = new boolean[getNodeID()];
+		queue.add(nodeID);
+		
+		while(!queue.isEmpty()){
+			//Get nodeID, from nodeID get the node, from node print the data
+			int nodeIndex = queue.remove().getData();
+			System.out.println( nodeList.get( nodeIndex ).getData() + " ---> "); //TODO: change return type for Queuev1.remove()
+			visited[nodeIndex] = true;
+			
+			LinkedList<Integer> edgeList = edge[nodeIndex];
+			for(Integer edgeId : edgeList){
+				if(!visited[edgeId])
+					queue.add(edgeId);
+			}
+		}
 	}
 	
 	public void dfs(){
-		
-	}
-	
-	public void insert(GraphNode graphNode){
 		
 	}
 	
@@ -44,7 +84,24 @@ public class Graphv1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Graphv1 graph = new Graphv1(1);
+		
+		graph.addNode(2);
+		graph.addNode(3);
+		graph.addNode(4);
+		graph.addNode(5);
+		graph.addNode(6);
+		graph.addNode(7);
 
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 2);
+		graph.addEdge(0, 3);
+		graph.addEdge(1, 4);
+		graph.addEdge(2, 5);
+		graph.addEdge(4, 5);
+		graph.addEdge(5, 6);
+		
+		graph.bfs(1);
 	}
 
 }
