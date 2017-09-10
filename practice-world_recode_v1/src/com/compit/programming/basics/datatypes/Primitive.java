443fd632a8808376ed30f9ad730559b3014b75f6
Mehul Chaudhari <chaudharimehul@gmail.com>
2017-09-09 19:00:00 -0700

    Few more primitive data types

--- practice-world_recode_v1/src/com/compit/programming/basics/datatypes/Primitive.java
+++ practice-world_recode_v1/src/com/compit/programming/basics/datatypes/Primitive.java
@@ -1,5 +1,8 @@
 package com.compit.programming.basics.datatypes;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Primitive {
 
 	/*
@@ -16,13 +19,64 @@ public class Primitive {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean b = false;
-		System.out.printf("--> boolean with %%b option - %b \n", b);
-		
+		boolean bol = false;
+		System.out.printf("--> boolean with %%b option - %b \n", bol);
+
+		byte b = 67;
+		System.out.printf("--> byte with %%d print option - %d \n", b);
+		System.out.printf("--> byte with %%c print option - %c \n", b);		
+
+		System.out.printf(" %7s %7s %7s %s \n", Byte.TYPE, Byte.SIZE, Byte.MIN_VALUE, Byte.MAX_VALUE);
+
+		short s = 1000;
+		System.out.printf("--> short with %%d print option - %d \n", s);
+
 		int i = 'm';
 		System.out.printf("--> int with %%d print option - %d \n", i);
 		System.out.printf("--> int with %%c print option - %c \n", i);
+
+		char c = 65;	
+		//System.out.printf("--> char with %%d print option - %d \n", c); - this conversation is not working
+		System.out.printf("--> char with %%c print option - %c \n", c);
+		System.out.printf("--> char with %%d print option - %d \n", (int)c ); 
+
+		int[] array = new int[10];
+
+		Random r = new Random();
+		for(i = 9 ; i > 0 ; i--){
+			//array[9-i] = i;
+			array[i] = r.nextInt(50);
+
+		}
+
+		for(i = 0 ; i < array.length ; i++){
+			for(int k = i ; k < array.length ; k++){
+				if(array[i] > array[k]){					
+					int temp = array[i];
+					array[i] = array[k];
+					array[k] = temp;
+				}
+			}
+		}
+		//Arrays.sort(array);
+
+		for(int k : array){
+			System.out.printf(" %4d| ", k);
+		}
+		System.out.println("");
+
+		long l = 2000000;
+		System.out.printf("--> long with %%d print option - %d \n" , l);
+		System.out.printf("--> long with %%c print option - %c \n" , (char)l);
+	
+		float f = 67.5f;
 		
+		System.out.printf("--> float with %%d print option - %d \n", (int)f);
+		System.out.printf("--> float with %%c print option - %c \n", (char)f);
+		System.out.printf("float %%f - %f \n", f);		
+		
+
+
 	}
 
 }
